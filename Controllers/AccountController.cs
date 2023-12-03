3bf814c2beef9420728739e7d8b460bb91bb3bc9
Enes Doğanay <doganayenes.94@gmail.com>
2023-12-03 16:11:47 +0300

    First commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+    public IActionResult Signup()
+    {
+        return View();
+    }
+}
